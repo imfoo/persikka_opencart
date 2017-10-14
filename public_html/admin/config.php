cc27e8a0615db751a9489951d4135b9d4d975671
EC2 Default User <ec2-user@ec2-34-201-3-220.compute-1.amazonaws.com>
2017-10-14 14:41:35 +0000

    initial import

---
+++ public_html/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/admin/');
+define('HTTP_CATALOG', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/');
+define('HTTP_IMAGE', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/admin/');
+define('HTTPS_CATALOG', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/');
+define('HTTPS_IMAGE', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ec2-user/public_html/admin/');
+define('DIR_SYSTEM', '/home/ec2-user/public_html/system/');
+define('DIR_DATABASE', '/home/ec2-user/public_html/system/database/');
+define('DIR_LANGUAGE', '/home/ec2-user/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/ec2-user/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/ec2-user/public_html/system/config/');
+define('DIR_IMAGE', '/home/ec2-user/public_html/image/');
+define('DIR_CACHE', '/home/ec2-user/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/ec2-user/public_html/download/');
+define('DIR_LOGS', '/home/ec2-user/public_html/system/logs/');
+define('DIR_CATALOG', '/home/ec2-user/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc_user');
+define('DB_PASSWORD', 'i-09f620e6c926d1478');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
---
+++ public_html/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/');
+define('HTTP_IMAGE', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/image/');
+define('HTTP_ADMIN', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/');
+define('HTTPS_IMAGE', 'http://ec2-34-201-3-220.compute-1.amazonaws.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ec2-user/public_html/catalog/');
+define('DIR_SYSTEM', '/home/ec2-user/public_html/system/');
+define('DIR_DATABASE', '/home/ec2-user/public_html/system/database/');
+define('DIR_LANGUAGE', '/home/ec2-user/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/ec2-user/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ec2-user/public_html/system/config/');
+define('DIR_IMAGE', '/home/ec2-user/public_html/image/');
+define('DIR_CACHE', '/home/ec2-user/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/ec2-user/public_html/download/');
+define('DIR_LOGS', '/home/ec2-user/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc_user');
+define('DB_PASSWORD', 'i-09f620e6c926d1478');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
